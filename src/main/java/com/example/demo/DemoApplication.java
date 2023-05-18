25c036fe3517fe5fcef5c7f3734aab85f66ec243
Burr Sutter <burrsutter@gmail.com>
2023-05-18 15:33:45 -0400

    SER93 feature

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -34,8 +34,8 @@ public class DemoApplication {
       simpleDateFormat = new SimpleDateFormat(pattern);
       String stringNow = simpleDateFormat.format(new Date());
 
-      System.out.println("Hola: " + cnt);
-      return "Hola " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      System.out.println("Wassup: " + cnt);
+      return "Wassup " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
