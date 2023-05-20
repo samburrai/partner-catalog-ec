d494f77eaaaf4b913fa2c8b9357296de98a13360
Burr Sutter <burrsutter@gmail.com>
2023-05-20 15:20:16 -0400

    SER96

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -41,8 +41,8 @@ public class DemoApplication {
       simpleDateFormat = new SimpleDateFormat(pattern);
       String stringNow = simpleDateFormat.format(new Date());
 
-      System.out.println("SER95: " + cnt);
-      return "SER95 " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      System.out.println("SER96: " + cnt);
+      return "SER96 " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
