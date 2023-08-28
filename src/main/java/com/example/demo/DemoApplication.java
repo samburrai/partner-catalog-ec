dbecb1747c8f033ace94c3635e62e8694debba60
Burr Sutter <burrsutter@gmail.com>
2023-08-28 10:10:57 -0400

    SER101 feature

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -41,8 +41,8 @@ public class DemoApplication {
       simpleDateFormat = new SimpleDateFormat(pattern);
       String stringNow = simpleDateFormat.format(new Date());
 
-      System.out.println("SER401");
-      return "SER401 " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      System.out.println("SER101");
+      return "SER101 " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
