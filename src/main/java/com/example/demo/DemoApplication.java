7f1a2394bdd0459a1ef8152648f0e9ddf4b72388
Burr Sutter <burrsutter@gmail.com>
2023-10-09 06:39:30 +0200

    SER108 requirement updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -41,8 +41,8 @@ public class DemoApplication {
       simpleDateFormat = new SimpleDateFormat(pattern);
       String stringNow = simpleDateFormat.format(new Date());
 
-      System.out.println("SER107");
-      return "SER107 " + cnt++ + " " + stringNow + " on " + HOSTNAME;
+      System.out.println("SER108");
+      return "SER108 " + cnt++ + " " + stringNow + " on " + HOSTNAME;
     }
 
     public static void main(String[] args) {
